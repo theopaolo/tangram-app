78c80b7f28b0c92a525231a0e975899f20907c7a
lambdaka <lambdaka@gmail.com>
2025-10-11 12:54:11 +0200

    css

--- src/routes/puzzles/+page.svelte
+++ src/routes/puzzles/+page.svelte
@@ -462,8 +462,8 @@ function triggerConfetti() {
     right:8%;
   }
   .puzzle-card:nth-child(even) {
-  background-color: rgb(248, 248, 248);
-}
+    background-color: rgb(248, 248, 248);
+  }
 </style>
 
 <!-- Title -->
@@ -490,7 +490,7 @@ function triggerConfetti() {
 <!-- <div class="p-5 mt-[90px]"> -->
   <div class="">
     {#if allPuzzlesCompleted}
-      <div onclick={triggerConfetti} class="text-center absolute top-[100px] m-auto left-0 right-0 z-1">
+      <div onclick={triggerConfetti} class="text-center absolute top-[90px] m-auto left-0 right-0 z-1">
         <p>Bravo tu as completé les 7 tangrams  !<br/>Télécharge un fond d'écran !</p>
         <a href="/download/Chromogram_Wallpaper_MRAC.jpg" download class="block">
         <div class="w-[124px] h-[68px] m-auto relative mt-2">
